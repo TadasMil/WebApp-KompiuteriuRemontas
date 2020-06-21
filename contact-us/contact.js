94895fc9a15c3c878632f57a2d0dd4ac09051897
tadas <ttadas10@gmail.com>
2020-06-21 23:25:15 +0300

    Working on contact page

--- contact-us/contact.js
+++ contact-us/contact.js
@@ -1,7 +1,13 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+  var myLatLng = {lat: 54.699 , lng: 25.258};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
   });
 }
